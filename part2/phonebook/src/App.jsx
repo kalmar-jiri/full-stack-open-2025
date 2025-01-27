c0945272addb564165a384dcddb73272a9091ff0
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-01-27 08:46:42 +0100

    Exercises 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,15 @@ import phoneService from './services/phonebook';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchName, setSearchName] = useState('');
+  const [notification, setNotification] = useState(null);
+  const [notificationColor, setNotificationColor] = useState('green');
 
   useEffect(() => {
     phoneService.showPhonebook().then(initialContacts => setPersons(initialContacts));
@@ -26,9 +29,22 @@ const App = () => {
         const person = persons.find(p => p.name === newName);
         const updatedPerson = { ...person, number: newNumber };
 
-        phoneService.updateContact(person.id, updatedPerson).then(returnedContact => {
-          setPersons(persons.map(p => (p.id === person.id ? returnedContact : p)));
-        });
+        phoneService
+          .updateContact(person.id, updatedPerson)
+          .then(returnedContact => {
+            setPersons(persons.map(p => (p.id === person.id ? returnedContact : p)));
+            setNotification(`Updated number for ${person.name}`);
+            setTimeout(() => setNotification(null), 5000);
+          })
+          .catch(error => {
+            setNotificationColor('red');
+            setNotification(`Information about ${person.name} has already been removed from server`);
+            setPersons(persons.filter(p => p.id !== person.id));
+            setTimeout(() => {
+              setNotification(null);
+              setNotificationColor('green');
+            }, 5000);
+          });
       }
       setNewName('');
       setNewNumber('');
@@ -41,6 +57,8 @@ const App = () => {
         setPersons(persons.concat(returnedContact));
         setNewName('');
         setNewNumber('');
+        setNotification(`Added ${newPerson.name}`);
+        setTimeout(() => setNotification(null), 5000);
       });
     }
   };
@@ -51,6 +69,12 @@ const App = () => {
     if (window.confirm(`Delete ${personToDelete.name}?`)) {
       phoneService.deleteContact(id).then(() => {
         setPersons(persons.filter(person => person.id !== id));
+        setNotificationColor('red');
+        setNotification(`Removed ${personToDelete.name}`);
+        setTimeout(() => {
+          setNotification(null);
+          setNotificationColor('green');
+        }, 5000);
       });
     }
   };
@@ -60,6 +84,7 @@ const App = () => {
   return (
     <div>
       <h1>PHONEBOOK ☎️</h1>
+      <Notification message={notification} color={notificationColor} />
       <Filter searchName={searchName} handleSearchChange={handleSearchChange} />
       <h2>Add a new 📝</h2>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
