0f1a974e481dcc03a45c7134497d8b9a97ee4f0a
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-02-01 18:05:54 +0100

    Create MongoDB model

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Note = require('./models/note');
 const cors = require('cors');
 
 app.use(cors());
@@ -43,7 +44,9 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (request, response) => {
@@ -87,7 +90,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
