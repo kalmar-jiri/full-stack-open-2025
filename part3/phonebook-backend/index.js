2e63cc62d31251fb1d563dbbc0c65dad1988a5d5
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-02-02 09:27:38 +0100

    Exercises 3.13.-3.14.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,29 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
-
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+const Person = require('./models/person');
 
 app.use(express.json());
 app.use(cors());
@@ -60,7 +38,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => response.json(persons));
 });
 
 app.get('/info', (request, response) => {
@@ -71,14 +49,8 @@ app.get('/info', (request, response) => {
         `);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = persons.find(p => p.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id).then(returnedPerson => res.json(returnedPerson));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -87,27 +59,21 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'Name or Number is missing.',
-    });
-  } else if (persons.some(p => p.name === body.name)) {
-    return response.status(400).json({
-      error: `${body.name} is already in the phonebook`,
-    });
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'name or number are missing' });
   }
-  const newPerson = {
-    id: Math.floor(Math.random() * 1000),
+
+  const newPerson = new Person({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(newPerson);
-  response.json(newPerson);
+  });
+
+  newPerson.save().then(returnedPerson => res.json(returnedPerson));
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
