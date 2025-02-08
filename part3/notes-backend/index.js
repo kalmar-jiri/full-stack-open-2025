8aa30c40072e6f2d706ab5d8dffc67a8cd7f396f
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-02-08 09:43:06 +0100

    ESlint incorporation

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -6,24 +6,6 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static('dist'));
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
-
 const requestLogger = (request, response, next) => {
   console.log(`Method: ${request.method}`);
   console.log(`Path: ${request.path}`);
