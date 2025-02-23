a3e3b4e8adee83c20a9a041207a4100cd57a4539
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-02-23 09:35:09 +0100

    create login router

--- part4/notes-backend/app.js
+++ part4/notes-backend/app.js
@@ -9,6 +9,7 @@ const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 mongoose.set('strictQuery', false);
 logger.info(`connecting to ${config.MONGODB_URI}`);
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
