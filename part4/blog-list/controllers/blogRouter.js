9aa8ee0c510e79e54dcc107e5168229af494438e
Jiri Kalmar <kalmar.jiri@gmail.com>
2025-02-24 16:19:02 +0100

    Exercise 4.17

--- part4/blog-list/controllers/blogRouter.js
+++ part4/blog-list/controllers/blogRouter.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   res.json(blogs);
 });
 
@@ -18,17 +19,22 @@ blogRouter.get('/:id', async (req, res) => {
 blogRouter.post('/', async (req, res) => {
   const body = req.body;
 
+  const user = await User.findOne({ username: 'admin' });
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ? body.likes : 0,
+    user: user.id,
   });
 
   if (!blog.title || !blog.url) {
     res.status(400).send({ error: 'title or url is missing' }).end();
   } else {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     res.status(201).json(savedBlog);
   }
 });
